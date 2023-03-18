c359d25904f092f26f823c10b2f3482933634693
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2023-03-18 10:33:57 +0100

    feat: created counter functionality

---
+++ lib/counter/view/counter_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../counter.dart';
+
+class CounterPage extends StatelessWidget {
+  /// {@macro counter_page}
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const CounterView(),
+    );
+  }
+}
